7120629f93f03655b5f60864e3dc1dbe8a585700
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-11-25 16:42:28 +0100

    Añadidas declaraciones

--- src/urjc/isi/union/UnionSetTest.java
+++ src/urjc/isi/union/UnionSetTest.java
@@ -6,20 +6,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+
 public class UnionSetTest {
 	
-	
+	Set a;
+	Set b;
+	Set c;
 
 	@Before
 	public void setUp() throws Exception {
+		a = new HashSet();
+		b = new HashSet();
+		c = new HashSet();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		a = null;
+		b = null;
+		c = null;
 	}
 
 	@Test
-	public void test() {
+	public void testHappyPath1elem() {
 		fail("Not yet implemented");
 	}
 
