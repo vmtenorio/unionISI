96d4b7de9205dbd946a2c802862f54c9b81d868c
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-11-25 11:41:56 +0100

    Initial Commit

---
+++ src/Union.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Union {
+	
+	
+	/**
+	 * @return	New object (different from a and b) of class Vector that
+	 *			contains the union set (NO elements can be repeated) of a and b
+	 * @param	a Vector containing elements, including null. It is not a set,
+	 *			so it can have repeated elements, and they can be of different
+	 *			classes / types
+	 * @param	b Vector containing elements, including null. It is not a set,
+	 *			so it can have repeated elements, and they can be of different
+	 *			classes / types
+	 * @throws	NullPointerException If a or b is null
+	 */
+	public static Vector union (Vector a, Vector b){
+		return null;
+	}
+}
