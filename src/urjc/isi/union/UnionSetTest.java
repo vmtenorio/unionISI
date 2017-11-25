fa407c750c8eebc1e974b509310db67969631152
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-11-25 16:53:17 +0100

    Test 1: HappyPath con 1 elemento

--- src/urjc/isi/union/UnionSetTest.java
+++ src/urjc/isi/union/UnionSetTest.java
@@ -6,21 +6,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+
 public class UnionSetTest {
 	
-	
+	Set a;
+	Set b;
+	Set c;
 
 	@Before
 	public void setUp() throws Exception {
+		a = new HashSet();
+		b = new HashSet();
+		c = new HashSet();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		a = null;
+		b = null;
+		c = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHappyPath() {
+		a.add(1);
+		b.add(2);
+		c.add(1);
+		c.add(2);
+		assertTrue("Fallo al unir vectores", c.equals(Union.unionSet(a, b)));
+		// No puedo hacer código que pruebe solo esto como antes, pues no hay get, por lo que continuo
 	}
 
 }
